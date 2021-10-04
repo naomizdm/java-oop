60aebd6004d24a44c2232b435d4d4347e8ce1a37
Naomi O. Zidomi <90383552+naomizdm@users.noreply.github.com>
2021-10-03 22:22:30 -0700

    Update T07AFraction.java

--- 07. Methods/Tugas/T07AFraction.java
+++ 07. Methods/Tugas/T07AFraction.java
@@ -1,7 +1,51 @@
 // File: T07AFraction.java
 public class T07AFraction {
   // bagian ide/ algoritma
-  public static void main(String[] args) {
-    // method utama untuk melaukan testcase
-  }
+  
+  // method menambahkan pecahan 1 dengan pecahan 2
+    static void sumFraction(int e1, int d1, int e2, int d2) {
+        int es, ds, pembagi;
+        es = e1 * d2 + e2 * d1;
+        ds = d1 * d2;
+        pembagi = fpb(es, ds);
+        es = es / pembagi;
+        ds = ds / pembagi;
+        printFraction(es, ds);
+    }
+
+    //method mengalikan pecahan 1 dengan pecahan 2
+    static void productFraction(int e1, int d1, int e2, int d2) {
+        int ep, dp;
+        ep = e1 * e2;
+        dp = d1 * d2;
+        int pembagi = fpb(ep, dp);
+        ep = ep / pembagi;
+        dp = dp / pembagi;
+        printFraction(ep, dp);
+    }
+
+    //method mencetak pecahan ke terminal
+    static void printFraction(int e, int d) {
+        System.out.println(e + " / " + d);
+    }
+
+    //method mencari fpb pembilang dan penyebut
+    static int fpb(int e, int d) {
+        int Temp;
+        while(d != 0) {
+            Temp = d;
+            d = e % d;
+            e = Temp;
+        }
+        return e;
+    }
+
+    public static void main(String[] args){
+        //method utama untuk melakukan Testcase
+        sumFraction(1, 2, 2, 3);
+        sumFraction(1, 3, 3, 4);
+        productFraction(1, 2, 2, 3);
+        productFraction(1, 4, 2, 3);
+        //sumFraction(1, 2, 1, 2);
+    }
 }
